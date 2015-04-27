04892f4b53b2504a86e4f57045963c2faa5ebbcc
EderSantana <eder@macbook.com>
2015-04-26 22:53:42 -0400

    Added pref_grid
    Added pref_grid method to plot preferred "layer bellow" weights

--- agnez/weight/__init__.py
+++ agnez/weight/__init__.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def grid2d(X, example_width=False, display_cols=False):
+def grid2d(X, example_width=False, display_cols=False, pad_row=1, pad_col=1):
     """Display weights in a nice grid
 
     This function assumes that each row of the X is an image weight to be
@@ -11,12 +11,18 @@ def grid2d(X, example_width=False, display_cols=False):
     Parameters
     ----------
     X : `numpy.array`
-         array with each filter to be transformed to an image on the rows
+        array with each filter to be transformed to an image on the rows
     example_width: int
-         defines the width of the images in the rows X if they are
-         not square
+        defines the width of the images in the rows X if they are
+        not square
+    display_cols: bool
+    pad_row: int
+        integer number of pixels between vertical neighbors
+    pad_col: int
+        integer number of pixels between horizontal neighbors
 
-    Borrowed from https://github.com/martinblom/py-sparse-filtering
+    Adapted from https://github.com/martinblom/py-sparse-filtering
+
     """
     m, n = X.shape
     if not example_width:
@@ -26,10 +32,9 @@ def grid2d(X, example_width=False, display_cols=False):
     if not display_cols:
         display_cols = int(np.sqrt(m))
     display_rows = int(np.ceil(m/display_cols))
-    pad = 1
     # Setup blank display
-    display_array = -np.ones((pad+display_rows * (example_height+pad),
-                              pad+display_cols * (example_width+pad)))
+    display_array = -np.ones((pad_row+display_rows * (example_height+pad_row),
+                              pad_col+display_cols * (example_width+pad_col)))
     # Copy each example into a patch on the display array
     curr_ex = 0
     for j in range(display_rows):
@@ -39,8 +44,8 @@ def grid2d(X, example_width=False, display_cols=False):
             # Copy the patch
             # Get the max value of the patch
             max_val = abs(X[curr_ex, :]).max()
-            i_inds = example_width*[pad+j * (example_height+pad)+q for q in range(example_height)]
-            j_inds = [pad+i * (example_width+pad)+q
+            i_inds = example_width*[pad_row+j * (example_height+pad_row)+q for q in range(example_height)]
+            j_inds = [pad_col+i * (example_width+pad_col)+q
                       for q in range(example_width)
                       for nn in range(example_height)]
             try:
@@ -53,3 +58,40 @@ def grid2d(X, example_width=False, display_cols=False):
             break
     visual = (display_array - display_array.min()) / (display_array.max() - display_array.min())
     return visual  # display_array
+
+
+def pref_grid(above, bellow, num_preferred=10, abs_value=True, pad_col=5):
+    """Display the weights that the layer above prefers on the layer below
+
+    This function looks for the `num_preferred` larger values on the layer
+    `above` and get their indexes. Those indexes are used to retrieve the
+    preferred weights on the layer `bellow`. After all, those preferred
+    vectors are organized with `meth`:grid2d.
+
+    Parameters
+    ----------
+    above : `numpy.array`
+        matrix with each filter to be transformed to an image on the rows
+    bellow : `numpy.array`
+        matrix with each filter to be transformed to an image on the rows
+    num_preferred: int
+        number of preferred weights to be plotted
+    abs_value: bool
+        if True chooses the preferred as the weights associated with
+        maximum absolute activation. Else, uses only the maximum positve
+        values.
+    pad_col: int
+        integer number of pixels between horizontal neighbors
+
+    """
+    idx = np.random.randint(above.shape[0], size=num_preferred)
+    R = np.abs(above[idx]) if abs_value else above[idx]
+    X = np.zeros((num_preferred**2, bellow.shape[1]))
+    for i, w in enumerate(R):
+        s = np.argsort(w)
+        prefs = s[:-num_preferred-1:-1]
+        first = i*num_preferred
+        last = (i+1)*num_preferred
+        X[first:last] = bellow[prefs]
+    visual = grid2d(X, pad_row=1, pad_col=pad_col)
+    return visual[:, pad_col-1:-pad_col+1]
