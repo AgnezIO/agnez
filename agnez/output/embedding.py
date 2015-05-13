eb50e4f7b2194bb9bc330bb78201803df9afb43e
EderSantana <eder@macbook.com>
2015-05-13 13:48:03 -0400

    embedding2d and the time series version are working fine and passing tests

--- agnez/output/embedding.py
+++ agnez/output/embedding.py
@@ -6,10 +6,12 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 
 
-def embedding2d(data, train_data=None, method=PCA(n_components=2)):
+def embedding2d(data, train_data=None, method=None):
     '''2D embedding for visualization
 
     '''
+    if method is None:
+        method = PCA(n_components=2)
     if train_data is None:
         ebd = method.fit_transform(data)
     else:
@@ -42,10 +44,37 @@ def embedding2dplot(data, labels):
     txts = []
     for i in range(10):
         # Position of each label.
-        xtext, ytext = np.median(data[data == i, :], axis=0)
+        xtext, ytext = np.median(data[labels == i, :], axis=0)
         txt = ax.text(xtext, ytext, str(i), fontsize=24)
         txt.set_path_effects([
             PathEffects.Stroke(linewidth=5, foreground="w"),
             PathEffects.Normal()])
         txts.append(txt)
+    return fig, ax, sc, txts
+
+
+def timeseries2d(data, train_data=None, method=None):
+    '''2D embedding for time-series
+
+    data: `numpy.array`
+        array with axes [time, batch, dim]
+    labels: `numpy.array`
+        array with axes [batch, ]
+
+    '''
+    t, b, d = data.shape
+    data = data.reshape((t*b, d))
+    ebd, mtd = embedding2d(data, train_data, method)
+    return ebd.reshape((t, b, -1))
+
+
+def timeseries2dplot(data, labels):
+    '''2D scatter plot of time series usng embeding2d
+
+    '''
+    t, b, d = data.shape
+    data = data.reshape((t*b, d))
+    labels = labels[np.newaxis].repeat(t, axis=0)
+    ebd, mtd = embedding2d(data)
+    fig, ax, sc, txts = embedding2dplot(ebd, labels)
     return fig, ax, sc, txts
--- tests/test_output.py
+++ tests/test_output.py
@@ -2,14 +2,24 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from agnez import embedding2d, embedding2dplot
+from agnez import embedding2d, embedding2dplot, timeseries2d, timeseries2dplot
 
 
 def test_embedding2d():
-    data = np.random.normal((5, 8))
+    data = np.random.normal(0, 1, (5, 8))
     labels = np.arange(5)
     ebd, mtd = embedding2d(data)
     assert ebd.shape == (5, 2)
 
     fig, ax, sc, txts = embedding2dplot(ebd, labels)
+    assert isinstance(fig, plt.Figure)
+
+
+def test_timesseries2d():
+    data = np.random.normal(0, 1, (5, 3, 8))
+    labels = np.arange(5)
+    ebd, mtd = timeseries2d(data)
+    assert ebd.shape == (5, 3, 2)
+
+    fig, ax, sc, txts = timeseries2dplot(ebd, labels)
     assert isinstance(fig, plt.Figure)
