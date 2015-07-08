d7ba1dcff0f8be4b91e04a197b93d22fd4b6d30a
EderSantana <eder@macbook.com>
2015-07-08 10:06:02 -0400

    Set Seaborn defaults

--- agnez/__init__.py
+++ agnez/__init__.py
@@ -4,6 +4,12 @@ __author__ = 'Eder Santana'
 __email__ = 'edercsjr+git@gmail.com'
 __version__ = '0.1.0'
 
+import seaborn as sns
+sns.set_style('dark')
+sns.set_palette('muted')
+sns.set_context("notebook", font_scale=1.5,
+                rc={"lines.linewidth": 2.5})
+
 from weight import *
 from inputs import *
 from output import *
