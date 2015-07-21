8b9d1e1319c86034fdb9dac19228ead767bbe1ab
EderSantana <eder@macbook.com>
2015-07-21 16:06:29 -0400

    Added params

--- agnez/weight/gaborfitting.py
+++ agnez/weight/gaborfitting.py
@@ -16,11 +16,11 @@ class GaborFit(Layer):
         init0 = initializations.get('zero')
         init1 = initializations.get('uniform')
         xydim = np.sqrt(output_dim)
-        x, y = np.meshgrid((np.linspace(-1, 1, xydim),)*2)
+        x, y = np.meshgrid(*(np.linspace(-1, 1, xydim),)*2)
         self.x = theano.shared(x.ravel().astype(floatX))
         self.y = theano.shared(y.ravel().astype(floatX))
-        self.x0 = init0((1,))
-        self.y0 = init0((1,))
+        self.x0 = init0((input_dim,))
+        self.y0 = init0((input_dim,))
         self.theta = init0((input_dim,))
         self.omega = init1((input_dim,))
         self.input = tensor.matrix()
@@ -28,19 +28,20 @@ class GaborFit(Layer):
             self.kappa = 2.5
         else:
             self.kappa = np.pi
+        self.params = [self.x0, self.y0, self.theta, self.omega]
 
     def _outter(self, t1, t2):
         return tensor.tensordot(t1, t2, axes=([], []))
 
     def get_output(self, train=False):
         rnorm = self.omega / np.sqrt(2*np.pi)*self.kappa
-        val = - self.omage**2 / (8 * self.kappa**2)
+        val = - self.omega**2 / (8 * self.kappa**2)
         dir1 = 4 * (self._outter(self.x, tensor.cos(self.theta)) +
                     self._outter(self.y, tensor.sin(self.theta)))**2
         dir2 = (-self._outter(self.x, tensor.sin(self.theta)) +
                 self._outter(self.y, tensor.cos(self.theta)))**2
-        ex = 1j * (self.omage * self._outter(tensor.cos(self.theta), self.x) +
-                   self.omage * self._outter(tensor.sin(self.theta), self.y))
+        ex = 1j * (self.omega * self._outter(tensor.cos(self.theta), self.x) +
+                   self.omega * self._outter(tensor.sin(self.theta), self.y))
         output = rnorm * tensor.exp(val * (dir1 + dir2)) * (tensor.exp(ex)
                                                             - tensor.exp(-self.kappa**2 / 2))
         return output
